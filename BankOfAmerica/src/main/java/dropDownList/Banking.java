8b2ffbca2a75f298c8d6d839e944d9b1db790737
Raheekhan <ibrahimkhan0894@gmail.com>
2017-09-10 10:06:55 -0400

    BankOfAmerica Modified

--- BankOfAmerica/src/main/java/dropDownList/Banking.java
+++ BankOfAmerica/src/main/java/dropDownList/Banking.java
@@ -15,6 +15,7 @@ public class Banking extends CommonAPI {
     @FindBy(how = How.XPATH, using = ".//*[@id='hp-section-2']/form/ul/li[1]/a")
     WebElement List_Banking_Button;
 
+    //// Checking ////
     @FindBy(how = How.NAME, using = "checking_")
     WebElement List_Banking_Checking;
 
@@ -24,59 +25,108 @@ public class Banking extends CommonAPI {
     @FindBy(how = How.ID, using = "go-button-zip-modal")
     WebElement Banking_Checking_Go;
 
+    //// Savings ////
     @FindBy(how = How.NAME, using = "savings")
     WebElement List_Banking_Savings;
 
+    //// CDs ////
     @FindBy(how = How.NAME, using = "cds")
     WebElement List_Banking_CDs;
 
+    //// Business Checking ////
     @FindBy(how = How.NAME, using = "business_checking")
     WebElement List_Banking_Business_Checking;
 
+    //// Student Banking ////
     @FindBy(how = How.NAME, using = "l2_banking_student_banking")
     WebElement List_Banking_Student_Banking;
 
+    //// Online Banking ////
     @FindBy(how = How.NAME, using = "online_banking")
     WebElement List_Banking_Online_Banking;
 
+    //// Mobile Banking ////
     @FindBy(how = How.NAME, using = "mobile_banking")
     WebElement List_Banking_Mobile_Banking;
 
+    //// More Products ////
     @FindBy(how = How.NAME, using = "12_banking_more_products")
     WebElement List_Banking_More_Products;
 
+    //// Preferred Rewards ////
     @FindBy(how = How.NAME, using = "preferred_rewards_")
     WebElement List_Banking_Preferred_Rewards;
 
+    //// Explore Popular Features ////
     @FindBy(how = How.NAME, using = "explore_popular_features")
     WebElement List_Banking_Explore_Popular_Features;
 
-    public void Checking() throws InterruptedException {
+    public void Banking_Button() throws InterruptedException {
         Thread.sleep(2000);
         WebElement element = List_Banking_Button;
         action.moveToElement(element).perform();
         Thread.sleep(2000);
+    }
+
+    public void Checking() throws InterruptedException {
+        Banking_Button();
         List_Banking_Checking.click();
         Banking_Checking_ZipCode.sendKeys("11373");
         Banking_Checking_Go.click();
         Thread.sleep(2000);
     }
 
-    public void Savings() {}
+    public void Savings() throws InterruptedException {
+        Banking_Button();
+        List_Banking_Savings.click();
+        Thread.sleep(2000);
+    }
 
-    public void CDs() {}
+    public void CDs() throws InterruptedException {
+        Banking_Button();
+        List_Banking_CDs.click();
+        Thread.sleep(2000);
+    }
 
-    public void Business_Checking() {}
+    public void Business_Checking() throws InterruptedException {
+        Banking_Button();
+        List_Banking_Business_Checking.click();
+        Thread.sleep(2000);
+    }
 
-    public void Student_Banking() {}
+    public void Student_Banking() throws InterruptedException {
+        Banking_Button();
+        List_Banking_Student_Banking.click();
+        Thread.sleep(2000);
+    }
 
-    public void Online_Banking() {}
+    public void Online_Banking() throws InterruptedException {
+        Banking_Button();
+        List_Banking_Online_Banking.click();
+        Thread.sleep(2000);
+    }
 
-    public void Mobile_Banking() {}
+    public void Mobile_Banking() throws InterruptedException {
+        Banking_Button();
+        List_Banking_Mobile_Banking.click();
+        Thread.sleep(2000);
+    }
 
-    public void More_Products() {}
+    public void More_Products() throws InterruptedException {
+        Banking_Button();
+        List_Banking_More_Products.click();
+        Thread.sleep(2000);
+    }
 
-    public void Preferred_Rewards() {}
+    public void Preferred_Rewards() throws InterruptedException {
+        Banking_Button();
+        List_Banking_Preferred_Rewards.click();
+        Thread.sleep(2000);
+    }
 
-    public void Explore_Popular_Features() {}
+    public void Explore_Popular_Features() throws InterruptedException {
+        Banking_Button();
+        List_Banking_Explore_Popular_Features.click();
+        Thread.sleep(2000);
+    }
 }
